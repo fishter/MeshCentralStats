45b3d4db182977319b3f456c9d819684693832ae
fishter <graeme.hilton@fishter.org.uk>
2025-08-29 11:41:58 +0200

    Update run_usage_stats.sh
    Tidy up uneeded variables (RM_BIN etc...)
    Add granularity options to analysis, hourly for daily stats and daily for monthly stats.

--- run_usage_stats.sh
+++ run_usage_stats.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 # Run usage analysis and save to text file.
-HOMEPATH=/home/ec2-user
+HOMEPATH=$HOME
 OUTPUT_PATH=${HOMEPATH}/meshcentral-files/domain/user-fosina_admin/NetUsage
 INPUT_PATH=${HOMEPATH}/meshcentral-data
-DATE_BIN=/usr/bin/date
-FIND_BIN=/usr/bin/find
-RM_BIN=/usr/bin/rm
 SCRIPT_NAME=${HOMEPATH}/AnalyseMeshCentralEventsdb_v4.py
 
-
 # create daily file, picking only recently updated db files to do so.
-SINCE=$(${DATE_BIN} +"%Y-%m-%d" --date='yesterday')
-BEFORE=$(${DATE_BIN} +"%Y-%m-%d" --date='today') # does not include today
+SINCE=$(date +"%Y-%m-%d" --date='yesterday')
+BEFORE=$(date +"%Y-%m-%d" --date='today') # does not include today
 OUTPUT_FILE=${OUTPUT_PATH}/NetUsage_Daily_${SINCE}.txt
-${FIND_BIN} ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(${DATE_BIN} --date='yesterday')" -exec ${SCRIPT_NAME} --before=${BEFORE} --since=${SINCE} --measurement=dec -o ${OUTPUT_FILE} {} +
+SCRIPT_OPTIONS="--before=${BEFORE} --since=${SINCE} --measurement=dec --granularity=60 -o ${OUTPUT_FILE}"
+find ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date --date='yesterday')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
 
 # create monthly file (on 1st, for previous month),
 # picking only db files modified in the last month - this may miss a few seconds of data at the start of a month, if the log rotation happens at exactly the wrong time.
 # and delete all the daily files from previous month as they are now in the monthly file.
-if [ $(${DATE_BIN} +"%d") == 01 ]
+if [ $(date +"%d") == 01 ]
 then
-    SINCE=$(${DATE_BIN} +"%Y-%m-01" --date="yesterday") # the first of the preceding month, if today is the first of the current month.
+    SINCE=$(date +"%Y-%m-01" --date="yesterday") # the first of the preceding month, if today is the first of the current month.
 #    BEFORE=$(${DATE_BIN} +"%Y-%m-%d" --date="today") # does not include today
-    OUTPUT_FILE = ${OUTPUT_PATH}/NetUsage_Monthly_${SINCE}--$(${DATE_BIN} +"%Y-%m-%d" --date="yesterday").txt
-    ${FIND_BIN} ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(${DATE_BIN} --date='last month')" -exec ${SCRIPT_NAME} --before=${BEFORE} --since=${SINCE} --measurement=dec -o ${OUTPUT_FILE} {} +
+    OUTPUT_FILE = ${OUTPUT_PATH}/NetUsage_Monthly_${SINCE}--$(date +"%Y-%m-%d" --date="yesterday").txt
+    SCRIPT_OPTIONS="--before=${BEFORE} --since=${SINCE} --measurement=dec --granularity=1440 -o ${OUTPUT_FILE}"
+    find ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date} --date='last month')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
     if [ $? == 0 ]
     then
-        ${RM_BIN} ${OUTPUT_PATH}/*Daily-$($DATE_BIN} +'%Y-%m' --date=yesterday)*
+        rm ${OUTPUT_PATH}/*Daily-$(date +'%Y-%m' --date=yesterday)-*.txt
     fi
 fi
+
 
