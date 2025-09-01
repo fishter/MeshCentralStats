4bf0e605456e12ee5835b303ea0eace9d21657a0
EC2 Default User <ec2-user@ip-172-31-23-222.eu-west-3.compute.internal>
2025-09-01 08:09:50 +0000

    Various fixes to the run_usage_stats.sh and to correctly specify the long input arguments for measurement and granularity.

--- run_usage_stats.sh
+++ run_usage_stats.sh
@@ -4,28 +4,30 @@ HOMEPATH=$HOME
 MC_USER=admin
 OUTPUT_PATH=${HOMEPATH}/meshcentral-files/domain/user-${MC_USER}/NetUsage
 INPUT_PATH=${HOMEPATH}/meshcentral-data
-SCRIPT_NAME=${HOMEPATH}/AnalyseMeshCentralEventsdb_v4.py
+SCRIPT_NAME=${HOMEPATH}/MeshCentralStats/AnalyseMeshCentralEventsdb_v4.py
 
 # create daily file, picking only recently updated db files to do so.
 SINCE=$(date +"%Y-%m-%d" --date='yesterday')
 BEFORE=$(date +"%Y-%m-%d" --date='today') # does not include today
 OUTPUT_FILE=${OUTPUT_PATH}/NetUsage_Daily_${SINCE}.txt
-SCRIPT_OPTIONS="--before=${BEFORE} --since=${SINCE} --measurement=dec --granularity=60 -o ${OUTPUT_FILE}"
+SCRIPT_OPTIONS="--before=${BEFORE} --since=${SINCE} -mdec -g60 -o ${OUTPUT_FILE}"
+#echo  ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date --date='yesterday')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
 find ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date --date='yesterday')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
 
 # create monthly file (on 1st, for previous month),
 # picking only db files modified in the last month - this may miss a few seconds of data at the start of a month, if the log rotation happens at exactly the wrong time.
-# and delete all the daily files from previous month as they are now in the monthly file.
+# and delete all the daily files from the before previous month.
 if [ $(date +"%d") == 01 ]
 then
     SINCE=$(date +"%Y-%m-01" --date="yesterday") # the first of the preceding month, if today is the first of the current month.
 #    BEFORE=$(${DATE_BIN} +"%Y-%m-%d" --date="today") # does not include today
-    OUTPUT_FILE = ${OUTPUT_PATH}/NetUsage_Monthly_${SINCE}--$(date +"%Y-%m-%d" --date="yesterday").txt
+    OUTPUT_FILE=${OUTPUT_PATH}/NetUsage_Monthly_${SINCE}--$(date +"%Y-%m-%d" --date="yesterday").txt
     SCRIPT_OPTIONS="--before=${BEFORE} --since=${SINCE} --measurement=dec --granularity=1440 -o ${OUTPUT_FILE}"
-    find ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date} --date='last month')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
+    # echo ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date --date='last month')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
+    find ${INPUT_PATH} -name meshcentral-events.db\* -newermt "$(date --date='last month')" -exec ${SCRIPT_NAME} ${SCRIPT_OPTIONS} {} +
     if [ $? == 0 ]
     then
-        rm ${OUTPUT_PATH}/*Daily-$(date +'%Y-%m' --date=yesterday)-*.txt
+        rm ${OUTPUT_PATH}/NetUsage_Daily_$(date +'%Y-%m' --date='-6 weeks')-*.txt 2> /dev/null
     fi
 fi
 
